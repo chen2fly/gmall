7e88b1668f6616b9a9108b1551f78f24e996b7b7
YUZE <chenyuze2010@163.com>
2020-06-25 21:19:03 +0800

    1232

--- gmall-common/src/main/scala/com/atguigu/dw/gmall/mock/util/ESUtil.scala
+++ gmall-common/src/main/scala/com/atguigu/dw/gmall/mock/util/ESUtil.scala
@@ -1,13 +1,15 @@
 package com.atguigu.dw.gmall.mock.util
 
+import java.{lang, util}
+
 import io.searchbox.client.config.HttpClientConfig
 import io.searchbox.client.{JestClient, JestClientFactory}
-import io.searchbox.core.{Bulk, Index}
+import io.searchbox.core.{Bulk, Index, Search, SearchResult}
 
 object ESUtil {
-    val esUrl = "http://hadoop201:9200"
-    val factory = new JestClientFactory
-    val conf: HttpClientConfig = new HttpClientConfig.Builder(esUrl)
+    private val esUrl = "http://localhost:9200"
+    private val factory = new JestClientFactory
+    private val conf: HttpClientConfig = new HttpClientConfig.Builder(esUrl)
             .multiThreaded(true)
             .maxTotalConnection(20)
             .connTimeout(10000)
@@ -60,6 +62,70 @@ object ESUtil {
                 case e => e.printStackTrace()
             }
         }
+    }
+    def getSaleDetailAndAggResultByAggField(date: String, keyword: String, startPage: Int, size: Int, aggField: String, aggSize: Int): Map[String, Any] = {
+
+        // 统计每个年龄购买情况
+        val searchDSL =
+            s"""
+               |{
+               |  "from": ${(startPage - 1) * size},
+               |  "size": $size,
+               |  "query": {
+               |    "bool": {
+               |      "filter": {
+               |        "term": {
+               |          "dt": "$date"
+               |        }
+               |      }
+               |      , "must": [
+               |        {"match": {
+               |          "sku_name": {
+               |            "query": "$keyword",
+               |            "operator": "and"
+               |          }
+               |        }}
+               |      ]
+               |    }
+               |  }
+               |  , "aggs": {
+               |    "groupby_$aggField": {
+               |      "terms": {
+               |        "field": "user_$aggField",
+               |        "size": $aggSize
+               |      }
+               |    }
+               |  }
+               |}
+         """.stripMargin
+
+        val search: Search = new Search.Builder(searchDSL)
+                .addIndex("gmall_sale_detail")
+                .addType("_doc")
+                .build()
+
+        val client: JestClient = ESUtil.getESClient
+        val result: SearchResult = client.execute(search)
+
+        // 1. 得到总数
+        val total: lang.Long = result.getTotal
+        // 2. 得到明细 (scala 集合)
+        val detailList = List[Map[String, Any]]() // 存储明细
+        val hits: util.List[SearchResult#Hit[util.HashMap[String, Any], Void]] = result.getHits(classOf[util.HashMap[String, Any]])
+        import scala.collection.JavaConversions._ // 要是使用 scala 的遍历凡是, 需要隐式转换
+        for (hit <- hits) {
+            val source: util.HashMap[String, Any] = hit.source
+            detailList.add(source.toMap)
+        }
+        // 3. 得到聚合结果
+        var aggMap = Map[String, Long]() // 存储聚合结果
+        val buckets = result.getAggregations.getTermsAggregation(s"groupby_$aggField").getBuckets
+        for (bucket <- buckets) {
+            aggMap += bucket.getKey -> bucket.getCount()
+        }
+
+        // 返回最终结果
+        Map("total" -> total, "aggMap" -> aggMap, "detail" -> detailList)
     }
 
 
